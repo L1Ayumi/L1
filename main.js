63b923af2273e6d9465984704bffe52debb8fb29
L1Ayumi <l1ayumikawa@gmail.com>
2022-05-19 16:49:38 +0900

    firebase W2

---
+++ main.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDm3AiN5TzPnx8zRUcrLRNGLQj7pIHJzZ0",
+    authDomain: "l1project-1e696.firebaseapp.com",
+    databaseURL: "https://l1project-1e696-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "l1project-1e696",
+    storageBucket: "l1project-1e696.appspot.com",
+    messagingSenderId: "570181742311",
+    appId: "1:570181742311:web:b6413c74bec20ef1773c42"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
